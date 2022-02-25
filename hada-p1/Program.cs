72293c139868dbf22cde935eebb8b81f2cacba39
Jose Vallino <jvvg5@gcloud.ua.es>
2022-02-25 14:09:02 +0100

    Program.cs ask user

--- hada-p1/Program.cs
+++ hada-p1/Program.cs
@@ -10,6 +10,29 @@ namespace hada_p1
     {
         static void Main(string[] args)
         {
+            char continuar;
+            double conversion;
+            do
+            {
+                Console.WriteLine("Unidad (Horas [h] / minutos [m] / segundos [s]): ");
+                char unidad = char.Parse(Console.ReadLine());
+                Console.WriteLine("Cantidad: ");
+                double cantidad = double.Parse(Console.ReadLine());
+                
+                if (unidad == 'm')
+                {
+                    conversion = HadaP1.Minutes2Seconds(cantidad);
+                    Console.WriteLine(cantidad + " minutos son " + conversion + " segundos");
+                }
+                else
+                {
+                    conversion = HadaP1.Seconds2Minutes(cantidad);
+                    Console.WriteLine(cantidad + " segundos son " + conversion + " minutos");
+                }
+              
+                Console.WriteLine("¿Realizar más conversiones? (s/n): ");
+                continuar = char.Parse(Console.ReadLine());
+            } while (continuar != 'n');
         }
     }
 }
